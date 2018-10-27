7e6f3951803a292488e86ade10562d25419a5c00
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-10-26 22:04:36 -0300

    adicionar handle para formatação de erros para json

--- bootstrap.php
+++ bootstrap.php
@@ -9,6 +9,7 @@
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 use Psr7Middlewares\Middleware\TrailingSlash;
+use Monolog\Logger;
 
 require './vendor/autoload.php';
 
@@ -29,6 +30,59 @@ $configs = [
  */
 $container = new \Slim\Container($configs);
 
+/**
+ * Converte os Exception dento da aplicação
+ * em respostas JSON
+*/
+$container['errorHandler'] = function ($c) {
+    return function ($request, $response, $exception) use ($c) {
+        $statusCode = $exception->getCode() ? $exception->getCode() : 500;
+
+        return $c['response']->withStatus($statusCode)
+            ->withJson(['message' => $exception->getMessage()], $statusCode);
+    };
+};
+
+/**
+ * Converte os Exceptions em Erros 405 - Not Allowed
+*/
+$container['notAllowedHandler'] = function ($c) {
+    return function ($request, $response, $methods) use ($c) {
+        return $c['response']
+            ->withStatus(405)
+            ->withHeader('Allow', implode(', ', $methods))
+            ->withHeader('Access-Control-Allow-Methods', implode(',', $methods))
+            ->withJson(['message' => 'Method not allowed; Method must be one of: '.implode(', ', $methods)], 405);
+    };
+};
+
+/**
+ * Converte os Exceptions em Erros 405 - Not Allowed
+*/
+$container['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        return $c['response']
+            ->withStatus(404)
+            ->withJson(['message' => 'Page not Found']);
+    };
+};
+
+/**
+ * Serviço de Logging em Arquivo
+*/
+$container['logger'] = function ($c) {
+    $logger = new Monolog\Logger('books-microservice');
+    $logfile = __DIR__ . '/log/books-microservice.log';
+    $stream = new Monolog\Handler\StreamHandler($logfile, Monolog\Logger::DEBUG);
+    $fingersCrossed = new Monolog\Handler\FingersCrossedHandler(
+        $stream,
+        Monolog\Logger::INFO
+    );
+    $logger->pushHandler($fingersCrossed);
+
+    return $logger;
+};
+
 $isDevMode = true;
 
 /**
@@ -40,13 +94,6 @@ $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src/Model
  * Array de configuração da nossa conexão
  * com o banco de dados
  */
-//$conn = array(
-//    'driver' => 'mysql',
-//    'host' => '127.0.0.1',
-//    'user' => 'api_slim',
-//    'pass' => 'api_slim',
-//    'dbname' => 'api_slim'
-//);
 $conn = array(
     'driver' => 'pdo_sqlite',
     'path' => './db.sqlite'
@@ -64,18 +111,8 @@ $entityManager = EntityManager::create($conn, $config);
 $container['em'] = $entityManager;
 
 /**
- * Converte os Exception dento da aplicação
- * em respostas JSON
-*/
-$container['errorHandler'] = function ($c) {
-    return function ($request, $response, $exception) use ($c) {
-        $statusCode = $exception->getCode() ? $exception->getCode() : 500;
-
-        return $c['response']->withStatus($statusCode)
-            ->withJson(['message' => $exception->getMessage()], $statusCode);
-    };
-};
-
+ * Application Instance
+ */
 
 $app = new \Slim\App($container);
 
